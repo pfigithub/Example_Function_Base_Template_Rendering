36aacfe7ee42943f5579e81eb85f34ded0dee955
pfigithub <pampam7072@gmail.com>
2022-12-15 12:53:30 +0330

    setting view for main

--- app1/views.py
+++ app1/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def index(req):
-    return render(req)
+    return render(req, 'website/index.html')
+
+def about(req):
+    return render(req, 'website/about.html')
+
+def contact(req):
+    return render(req, 'website/contact.html')
