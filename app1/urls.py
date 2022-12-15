07ae717cb2b12ff54428cde7c8b9dd26d8f27c3f
pfigithub <pampam7072@gmail.com>
2022-12-15 12:52:50 +0330

    setting app name and setting main urls

--- app1/urls.py
+++ app1/urls.py
@@ -1,6 +1,10 @@
 from app1.views import *
 from django.urls import path
 
+app_name = 'web'
+
 urlpatterns = [
-    path('home', index),
+    path('', index, name = 'index'),
+    path('about', about, name = 'about'),
+    path('contact', contact, name = 'contact')
 ]
