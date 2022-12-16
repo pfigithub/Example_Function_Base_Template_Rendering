9533343ce2502223ec22f2ec91215eacab384e39
pfigithub <pampam7072@gmail.com>
2022-12-16 16:21:04 +0330

    Update models.py
    create table for post in blog

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    # image
+    # category
+    # tag
+    # author
     title = models.CharField(max_length=250)
     content = models.TextField()
+    counted_views = models.IntegerField(default= 0)
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now= True)
+    published_date = models.DateTimeField(null=True)
