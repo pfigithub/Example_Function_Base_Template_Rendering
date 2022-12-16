1ff269dfc0197b46d779c09598f3508c65d0fd66
pfigithub <pampam7072@gmail.com>
2022-12-16 20:33:47 +0330

    Update admin.py
    making better admin panel about contact

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from app1.models import Contact
 
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    empty_value_display = "-empty-" 
+    list_display = ('name','email','created_date')  
+    list_filter = ('email',)
+    search_fields = ['name','subject','text']
+
+admin.site.register(Contact, ContactAdmin)
