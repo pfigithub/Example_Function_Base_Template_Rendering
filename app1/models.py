c33c11e4693f44e1365b4b53dc0ed2612c8dd494
pfigithub <pampam7072@gmail.com>
2022-12-16 16:18:43 +0330

    Update models.py
    create table for contact

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    subject = models.CharField(max_length=50)
+    text = models.TextField()
+    created_date = models.TimeField(auto_now_add=True)
+    updated_date = models.TimeField(auto_now=True)
