de8d1df30f96fc18f1d2ae5269d29bd3b75ccdb7
pfigithub <pampam7072@gmail.com>
2022-12-16 20:00:24 +0330

    Update admin.py
    added some features for showing admin panel better

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from blog.models import Post
 
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+# we can use this one(code in the blow) instead of that one(code up there with @) no diffrence
+# admin.site.register(Post, PostAdmin)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    empty_value_display = "-empty-" 
+    list_display = ('title','status','created_date','published_date','updated_date','counted_views')  
+    list_filter = ('status','counted_views')
+    search_fields = ['title','content']
+
