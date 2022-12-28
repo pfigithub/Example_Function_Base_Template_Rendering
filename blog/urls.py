5df970cf201793f2e535fd272d7397438f45f172
pfigithub <pampam7072@gmail.com>
2022-12-28 12:25:08 +0330

    Update urls.py
    add url category

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', blog_view, name = 'index_blog'),
+    path('category/<str:cat_name>', blog_view, name = 'category'),
     path('<int:pid>', blog_single, name = 'single_blog')
 ]
