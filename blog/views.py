cbbbd23a8c36e61085543962d78d6819a569d229
pfigithub <pampam7072@gmail.com>
2022-12-28 12:25:28 +0330

    Update views.py
    preparing category views

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
-def blog_view(req):
+def blog_view(req, cat_name= None):
     posts = Post.objects.filter(status=1)
+    if cat_name:
+        posts = posts.filter(category__name=cat_name)
     context = {'posts': posts}
     return render(req, 'blog/blog-home.html', context)
 
@@ -11,3 +13,11 @@ def blog_single(req, pid):
     post = get_object_or_404(Post, pk=pid, status =1)
     context = {'post': post}
     return render(req, 'blog/blog-single.html', context)
+
+
+def blog_category(req, cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts': posts}
+    return render(req, 'blog/blog-home.html', context)
+
