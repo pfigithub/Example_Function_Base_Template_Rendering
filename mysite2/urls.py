e9bd9036feb7c7a7bc5d28e8e1ab949d0fc41f04
pfigithub <pampam7072@gmail.com>
2023-01-02 16:29:36 +0330

    Update urls.py
    added sitemap in url

--- mysite2/urls.py
+++ mysite2/urls.py
@@ -17,12 +17,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from app1.sitemaps import StaticViewSitemap
+from blog.sitemaps import BlogSitemap
+sitemaps = {'static': StaticViewSitemap,'blog': BlogSitemap }
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app1.urls')),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap')
 ]
+
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
