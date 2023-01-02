626906e89423bac21db6cfd5c75a499989df0ffc
pfigithub <pampam7072@gmail.com>
2023-01-02 16:30:59 +0330

    Update models.py
    for sitemaps

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -28,5 +29,9 @@ class Post(models.Model):
 
     def __str__(self):
         return "{}-{}".format(self.title, self.counted_views)
+
+
+    def get_absolute_url(self):
+        return reverse ('blog:single_blog', kwargs={'pid':self.id})
 
 
