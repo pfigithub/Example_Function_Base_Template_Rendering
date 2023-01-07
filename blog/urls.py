7841810bfd969361250382aae623f6652b403fa7
pfigithub <pampam7072@gmail.com>
2023-01-07 11:41:34 +0330

    rss

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from blog.views import *
 from django.urls import path
+from blog.feeds import LatestEntriesFeed
 
 app_name = 'blog'
 
@@ -9,5 +10,6 @@ urlpatterns = [
     path('tag/<str:tag_name>', blog_view, name = 'tag'),
     path('author/<str:author_username>', blog_view, name = 'author'),
     path('search/', blog_search, name= 'search'),
-    path('<int:pid>', blog_single, name = 'single_blog')
+    path('<int:pid>', blog_single, name = 'single_blog'),
+    path('rss/feed/', LatestEntriesFeed())
 ]
