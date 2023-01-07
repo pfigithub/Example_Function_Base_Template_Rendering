ab3abc074694a8a1b5edd55b12ec67981fd4e1d4
pfigithub <pampam7072@gmail.com>
2023-01-07 08:48:34 +0330

    count comments

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Comment
 from blog.models import Category
 
 register = template.Library()
@@ -18,3 +18,9 @@ def postcategories():
     for name in categories:
         cat_dict[name] = posts.filter(category = name).count()
     return {'categories':cat_dict}
+
+
+@register.simple_tag(name='comments_count')
+def commentcount(pid):
+    return Comment.objects.filter(post= pid, approved=True).count()
+    context = {'post': post, 'comments':comments}
