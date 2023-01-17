5ff855680342d5f43215f06b746f93d80830bf50
pfigithub <pampam7072@gmail.com>
2023-01-17 10:43:21 +0330

    Update blog_tags.py

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 @register.inclusion_tag('blog/blog-latest-posts.html')
 def latestposts(arg = 6):
-    posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
+    posts = Post.objects.filter(status=1).order_by('-published_date')[:arg]
     return {'posts': posts}
 
 
