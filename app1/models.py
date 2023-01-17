5241ed8f3d555d72202bb0f9ee1a4a4ac320c234
pfigithub <pampam7072@gmail.com>
2023-01-17 10:42:16 +0330

    Update models.py

--- app1/models.py
+++ app1/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField()
-    subject = models.CharField(max_length=50)
+    subject = models.CharField(max_length=50, blank=True)
     text = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
